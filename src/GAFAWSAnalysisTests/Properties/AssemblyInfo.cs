c20d3bf0e267838c00d287dc11d0a018f30e31a7
Randy Regnier <rbregnier@gmail.com>
2010-10-02 21:14:32 -0500

    Create installer for 2.0RC1.

--- src/GAFAWSAnalysisTests/Properties/AssemblyInfo.cs
+++ src/GAFAWSAnalysisTests/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 
 [assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyFileVersion("2.0.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
